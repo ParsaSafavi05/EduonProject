3ed6f4a457b757bea8ebe609e482c16dff229b24
Parsa Safavi <parsasafavi1384@gmail.com>
2024-10-24 23:35:18 +0330

    added about page

---
+++ resources/about/index.php
@@ -0,0 +1,362 @@
+<?php
+
+$content = '
+
+
+<section class="education-area-two ptb-100">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="education-content">
+                <span class="top-title">یادگیری برای همه</span>
+                <h2>چرا در دوره های<span> EDUON </span>شرکت کنید؟</h2>
+                <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                 چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان 
+                که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
+                 کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                 کتابهای زیادی در شصت و سه درصد گذشته حال و آیند..</p>
+                <p>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul>
+                            <li>
+                                <i class="bx bx-check"></i>
+                                بی نهایت از همه چی
+                            </li>
+                            <li>
+                                <i class="bx bx-check"></i>
+                                بدون هزینه اضافه
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul>
+                            <li>
+                                <i class="bx bx-check"></i>
+                                بدون دردسر های تکنیکی
+                            </li>
+                            <li>
+                                <i class="bx bx-check"></i>
+                                انتقال رایگان
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="../courses/index" class="default-btn">
+                    همه دوره ها
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="education-img-wrap">
+                <div class="education-img-2">
+                    <img src="../../public/img/education-img-2.jpg" alt="Image">
+                </div>
+                <div class="education-img-3">
+                    <img src="../../public/img/education-img-3.jpg" alt="Image">
+                </div>
+                <div class="education-img-4">
+                    <img src="../../public/img/education-img-4.jpg" alt="Image">
+                </div>
+                <div class="education-shape-1">
+                    <img src="../../public/img/education-shape-1.jpg" alt="Image">
+                </div>
+                <div class="education-shape-2">
+                    <img src="../../public/img/education-shape-2.png" alt="Image">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<section class="affordable-area f5f6fa-bg-color pt-100 pb-70">
+<div class="container">
+    <div class="section-title">
+        <span>قیمت های مناسب</span>
+        <h2>با ما سود شما تضمین شده است</h2>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-affordable one">
+                <i class="flaticon-investment"></i>
+                <h3>صرفه جویی در وقت و هزینه</h3>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-affordable two">
+                <i class="flaticon-balance"></i>
+                <h3>آموزش متعادل با سبک زندگی شما</h3>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-affordable three">
+                <i class="flaticon-online-education"></i>
+                <h3>بدست آوردن تجربه ارزشمند</h3>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-affordable four">
+                <i class="flaticon-route"></i>
+                <h3>چیزی که شروع کردی رو تمام کن</h3>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<section class="event-area-two event-area-style ptb-100">
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-lg-6">
+            <div class="event-img">
+                <img src="../../public/img/event-img/event-img-5.png" alt="Image">
+                <div class="video-content">
+                    <a href="https://www.youtube.com/watch?v=iLS_YP1uEK8" class="video-btn popup-youtube">
+                        <i class="flaticon-play-button"></i>
+                    </a>
+                </div>
+                <div class="event-shape-1 rotated">
+                    <img src="../../public/img/event-img/event-shape-1.png" alt="Image">
+                </div>
+                <div class="event-shape-2">
+                    <img src="../../public/img/event-img/event-shape-2.png" alt="Image">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="section-title">
+                <span>کسب تجربه</span>
+                <h2>چطور میتونیم کمکت کنیم؟</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-sm-6">
+                    <div class="single-tutor one">
+                        <i class="flaticon-instructor-1"></i>
+                        <h3>یادگیری رو در رو</h3>
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="single-tutor two">
+                        <i class="flaticon-instructor"></i>
+                        <h3>استاد های با تجربه</h3>
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="single-tutor three">
+                        <i class="flaticon-certificate-1"></i>
+                        <h3>مدرک بین المللی</h3>
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="single-tutor four">
+                        <i class="flaticon-consulting"></i>
+                        <h3>تیم پشتیبانی 24 ساعته</h3>
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<section class="feedback-area feedback-area-three feedback-area-style ptb-100">
+<div class="container">
+    <div class="section-title">
+        <span>باز خورد</span>
+        <h2>دانشجو های ما چه میگویند</h2>
+    </div>
+    <div class="feedback-slider owl-theme owl-carousel">
+        <div class="feedback-item">
+            <i class="flaticon-quotation"></i>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                 چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان 
+                که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و</p>
+            <div class="feedback-title">
+                <img src="../../public/img/feedback-img/feedback-img-1.jpg" alt="Image">
+                <h3>مهتاب زمانی</h3>
+                <span>طراح</span>
+            </div>
+        </div>
+        <div class="feedback-item">
+            <i class="flaticon-quotation"></i>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                 چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان 
+                که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و</p>
+            <div class="feedback-title">
+                <img src="../../public/img/feedback-img/feedback-img-2.jpg" alt="Image">
+                <h3>آرین افشار</h3>
+                <span>بازاریابی</span>
+            </div>
+        </div>
+        <div class="feedback-item">
+            <i class="flaticon-quotation"></i>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی
+                 نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
+                 چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان 
+                که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و</p>
+            <div class="feedback-title">
+                <img src="../../public/img/feedback-img/feedback-img-3.jpg" alt="Image">
+                <h3>الهام یوسفی</h3>
+                <span>طراح</span>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<section class="teachers-area-three pt-100 pb-70">
+<div class="container">
+    <div class="section-title">
+        <span>استاد های ما</span>
+        <h2>اساتید بین المللی ما</h2>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-teachers">
+                <img src="../../public/img/teachers-img/teachers-img-1.jpg" alt="Image">
+                <div class="teachers-content">
+                    <ul>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank"><i
+                                    class="bx bxl-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank"><i
+                                    class="bx bxl-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank"><i
+                                    class="bx bxl-linkedin-square"></i></a>
+                        </li>
+                    </ul>
+                    <h3>ماهان دارمی</h3>
+                    
+                    <span>کامپیوتر و نرم افزار</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-teachers">
+                <img src="../../public/img/teachers-img/teachers-img-2.jpg" alt="Image">
+                <div class="teachers-content">
+                    <ul>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank"><i
+                                    class="bx bxl-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank"><i
+                                    class="bx bxl-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank"><i
+                                    class="bx bxl-linkedin-square"></i></a>
+                        </li>
+                    </ul>
+                    <h3>الهام یوسفی</h3>
+                    <span>زیاضی دان</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-teachers">
+                <img src="../../public/img/teachers-img/teachers-img-3.jpg" alt="Image">
+                <div class="teachers-content">
+                    <ul>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank"><i
+                                    class="bx bxl-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank"><i
+                                    class="bx bxl-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank"><i
+                                    class="bx bxl-linkedin-square"></i></a>
+                        </li>
+                    </ul>
+                    <h3>مهرداد گودرزی</h3>
+                    <span>برنامه نویس</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-teachers">
+                <img src="../../public/img/teachers-img/teachers-img-4.jpg" alt="Image">
+                <div class="teachers-content">
+                    <ul>
+                        <li>
+                            <a href="https://www.instagram.com/" target="_blank"><i
+                                    class="bx bxl-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank"><i
+                                    class="bx bxl-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank"><i
+                                    class="bx bxl-linkedin-square"></i></a>
+                        </li>
+                    </ul>
+                    <h3>الهام یوسفی</h3>
+                    <span>زبان انگلیسی</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<div class="partner-area f5f6fa-bg-color ptb-100">
+<div class="container">
+    <div class="partner-wrap owl-theme owl-carousel">
+        <div class="partner-item">
+            <a href="about.html">
+                <img src="../../public/img/partner-logo/partner-logo-1.png" alt="Image">
+            </a>
+        </div>
+        <div class="partner-item">
+            <a href="about.html">
+                <img src="../../public/img/partner-logo/partner-logo-2.png" alt="Image">
+            </a>
+        </div>
+        <div class="partner-item">
+            <a href="about.html">
+                <img src="../../public/img/partner-logo/partner-logo-3.png" alt="Image">
+            </a>
+        </div>
+        <div class="partner-item">
+            <a href="about.html">
+                <img src="../../public/img/partner-logo/partner-logo-4.png" alt="Image">
+            </a>
+        </div>
+        <div class="partner-item">
+            <a href="about.html">
+                <img src="../../public/img/partner-logo/partner-logo-5.png" alt="Image">
+            </a>
+        </div>
+    </div>
+</div>
+</div>
+
+';
+
+$this->layout($content);

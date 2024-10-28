3db9f1d8629075ebd0b58fa0f7283844269e622e
Parsa <parsasafavi1384@gmail.com>
2024-10-28 19:46:12 +0330

    added instructors

---
+++ resources/instructors/index.php
@@ -0,0 +1,231 @@
+<?php
+
+$content = '
+
+    <div class="page-title-area bg-2">
+        <div class="container">
+            <div class="page-title-content">
+                <h2>Instructors</h2>
+                <ul>
+                    <li>
+                        <a href="index.html">
+                            Home
+                        </a>
+                    </li>
+                    <li class="active">Courses instructors </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="teachers-area-three ptb-100">
+        <div class="container">
+            <div class="section-title">
+                <span>Our Teachers</span>
+                <h2>Our international teachers</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-1.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Earl McGowan</h3>
+                            <span>IT & Software</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-2.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Chris Miller</h3>
+                            <span>Mathematic</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-3.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Mark Dent</h3>
+                            <span>Programmer</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-4.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Lena Bodie</h3>
+                            <span>English</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-5.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Fred aaD. Iles</h3>
+                            <span>Data science</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-6.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Nancy Stout</h3>
+                            <span>Accounting</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-7.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Pedro T. Vaca</h3>
+                            <span>Finance</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-teachers">
+                        <img src="../../public/img/teachers-img/teachers-img-8.jpg" alt="Image">
+                        <div class="teachers-content">
+                            <ul>
+                                <li>
+                                    <a href="https://www.instagram.com/" target="_blank"><i
+                                            class="bx bxl-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank"><i
+                                            class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/" target="_blank"><i
+                                            class="bx bxl-linkedin-square"></i></a>
+                                </li>
+                            </ul>
+                            <h3>Ann H. Howard</h3>
+                            <span>Marketing</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12">
+                    <div class="pagination-area">
+
+                        <span class="page-numbers current" aria-current="page">1</span>
+                        <a href="instructors.html" class="page-numbers">2</a>
+                        <a href="instructors.html" class="page-numbers">3</a>
+                        <a href="instructors.html" class="page-numbers">4</a>
+                        <a href="instructors.html" class="next page-numbers">
+                            <i class="bx bx-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+';
+
+$this->layout($content);
